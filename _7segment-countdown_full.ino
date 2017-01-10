252b68a684ebf0a1d51a3c1a18253d170b3a0a2e
Markus Pöschl <gadrecs@users.noreply.github.com>
2017-01-10 22:14:28 +0100

    Add files via upload
    First Draw of Arduino-Sketch for serving the large 7 Segment - Displays,  > using the sparkfun large-LED-Drivers
    Not working fully. -> Large-LEDS not working, Countdown loop not exiting to main loop ...
    
    Need help

---
+++ _7segment-countdown_full.ino
@@ -0,0 +1,213 @@
+// 7-Segment-Countdown with external triggers
+ 
+//GPIO declarations
+//-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+byte segmentLatch = 5;
+byte segmentClock = 6;
+byte segmentData = 7;
+
+const int buttonyel = 2;     // the number of the pushbutton pin
+const int buttongre = 3;
+
+const int yelPin =  13;      // the number of the LED pin
+const int grePin =  12;
+const int redPin =  11;
+
+//definiere Zeiten:
+const int lang = 240;
+const int kurz = 120;
+const int prepare = 10;
+
+int stopstate = 0;
+int counttime = 0;
+int buttonStateyel = 0;         // variable for reading the pushbutton status
+int buttonStategre = 0;
+int x = 0;
+int start = 0;
+
+
+
+void setup()
+{
+  Serial.begin(9600);
+  Serial.println("Outdevice Device is starting");
+  pinMode(segmentClock, OUTPUT);
+  pinMode(segmentData, OUTPUT);
+  pinMode(segmentLatch, OUTPUT);
+  pinMode(yelPin, OUTPUT);
+  pinMode(grePin, OUTPUT);
+  pinMode(redPin, OUTPUT);
+  // initialize the pushbutton pin as an input:
+  pinMode(buttonyel, INPUT);
+  pinMode(buttongre, INPUT);
+
+  digitalWrite(segmentClock, LOW);
+  digitalWrite(segmentData, LOW);
+  digitalWrite(segmentLatch, LOW);
+
+  postNumber(0,0);
+  postNumber(0,0);
+  postNumber(0,0);
+  delay(2000);
+
+}
+
+void loop()
+  {
+    stopstate = 0;
+    getStart();
+    if (start == 1)
+      {
+      countDown();
+      int start = 0;
+      }
+    else return;
+  }
+
+
+void getStop()
+  {
+  // read the state of the pushbutton value:
+  buttonStateyel = digitalRead(buttonyel);
+  buttonStategre = digitalRead(buttongre);
+   
+    if (buttonStategre == HIGH && buttonStateyel == HIGH)
+    {
+      // STOP ausgelöst
+      stopstate = 1;
+      counttime = 0;
+      Serial.print("Stopstate: ");
+      Serial.println(stopstate); //for debugging
+    }
+    return;
+  } 
+
+
+
+void getStart()
+  {
+  // read the state of the pushbutton value:
+  buttonStateyel = digitalRead(buttonyel);
+  buttonStategre = digitalRead(buttongre);
+   
+    if (buttonStategre == HIGH && buttonStateyel == LOW)
+    {
+      // LANGES Programm ausgelöst
+      int counttime = 4 * lang; 
+      Serial.println(counttime); //for debugging
+      start = 1;
+    }
+    if (buttonStategre == LOW && buttonStateyel == HIGH)
+    {
+      // kurzes Programm ausgelöst
+      int counttime = 4 * kurz; 
+      Serial.println(counttime); //for debugging
+      start = 1;
+    }
+    return;
+  } 
+
+
+void showNumber(float value)   //Takes a number and displays the numbers. Displays absolute value (no negatives)
+  {
+    int number = abs(value); //Remove negative signs and any decimals
+
+    Serial.print("number: ");
+    Serial.println(number);
+
+    for (byte x = 0 ; x < 3 ; x++)
+      {
+      int remainder = number % 10;
+      postNumber(remainder, false);
+      Serial.print("remainder: ");
+      Serial.println(remainder);
+      number /= 10;
+      }
+      //Latch the current segment data
+      digitalWrite(segmentLatch, LOW);
+      digitalWrite(segmentLatch, HIGH); //Register moves storage register on the rising edge of RCK
+  }
+
+  
+void postNumber(byte number, boolean decimal)  //Given a number, or '-', shifts it out to the display
+  {
+  Serial.print("postNumber |");
+  //    -  A
+  //   / / F/B
+  //    -  G
+  //   / / E/C
+  //    -. D/DP
+
+  #define a  1<<0
+  #define b  1<<6
+  #define c  1<<5
+  #define d  1<<4
+  #define e  1<<3
+  #define f  1<<1
+  #define g  1<<2
+  #define dp 1<<7
+
+  byte segments;
+
+  switch (number)
+  {
+    case 1: segments = b | c; break;
+    case 2: segments = a | b | d | e | g; break;
+    case 3: segments = a | b | c | d | g; break;
+    case 4: segments = f | g | b | c; break;
+    case 5: segments = a | f | g | c | d; break;
+    case 6: segments = a | f | g | e | c | d; break;
+    case 7: segments = a | b | c; break;
+    case 8: segments = a | b | c | d | e | f | g; break;
+    case 9: segments = a | b | c | d | f | g; break;
+    case 0: segments = a | b | c | d | e | f; break;
+    case ' ': segments = 0; break;
+    case 'c': segments = g | e | d; break;
+    case '-': segments = g; break;
+  }
+
+  if (decimal) segments |= dp;
+
+  //Clock these bits out to the drivers
+  for (byte x = 0 ; x < 8 ; x++)
+    
+    digitalWrite(segmentClock, LOW);
+    digitalWrite(segmentData, segments & 1 << (7 - x));
+    digitalWrite(segmentClock, HIGH); //Data transfers to the register on the rising edge of SRCK
+  }
+
+
+
+void countDown()
+
+  {
+  Serial.print("Start Countdown...");
+  Serial.print("prepare !");
+  for (int prep = prepare; prep > -1; prep--)
+  {
+    showNumber(prep); //Test pattern
+    Serial.println(prep); //For debugging
+    delay(1000);
+    getStop();
+    if (stopstate == 1)
+      break;
+  }
+  
+  Serial.print("count !");
+  for (int count = lang; count > -1; count--)
+  {
+    showNumber(count); //Test pattern
+    Serial.print("counttime: ");
+    Serial.println(count); //For debugging
+    delay(250);
+    //getStop();
+    //if (stopstate == 1)
+    //  break;
+    if (count == 0)
+      stopstate = 1;
+      break;
+  }
+  }
+ // while (stopstate != 0);
+ // return;
+ // }
