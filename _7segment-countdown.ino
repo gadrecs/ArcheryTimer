7a51bcecc6f356f0bf6fde9d6ed69ae1af95b08b
Markus Pöschl <gadrecs@users.noreply.github.com>
2017-01-10 23:01:13 +0100

    Add files via upload
    this one works ... but no input / sensor starts ...

---
+++ _7segment-countdown.ino
@@ -0,0 +1,172 @@
+    /*
+ Controlling large 7-segment displays
+ By: Nathan Seidle
+ SparkFun Electronics
+ Date: February 25th, 2015
+ License: This code is public domain but you buy me a beer if you use this and we meet someday (Beerware license).
+
+ This code demonstrates how to post two numbers to a 2-digit display usings two large digit driver boards.
+
+ Here's how to hook up the Arduino pins to the Large Digit Driver IN
+
+ Arduino pin 6 -> CLK (Green on the 6-pin cable)
+ 5 -> LAT (Blue)
+ 7 -> SER on the IN side (Yellow)
+ 5V -> 5V (Orange)
+ Power Arduino with 12V and connect to Vin -> 12V (Red)
+ GND -> GND (Black)
+
+ There are two connectors on the Large Digit Driver. 'IN' is the input side that should be connected to
+ your microcontroller (the Arduino). 'OUT' is the output side that should be connected to the 'IN' of addtional
+ digits.
+
+ Each display will use about 150mA with all segments and decimal point on.
+
+*/
+
+//GPIO declarations
+//-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+byte segmentClock = 6;
+byte segmentLatch = 5;
+byte segmentData = 7;
+
+//-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+
+void setup()
+{
+  Serial.begin(9600);
+  Serial.println("Large Digit Driver Example");
+
+  pinMode(segmentClock, OUTPUT);
+  pinMode(segmentData, OUTPUT);
+  pinMode(segmentLatch, OUTPUT);
+
+  digitalWrite(segmentClock, LOW);
+  digitalWrite(segmentData, LOW);
+  digitalWrite(segmentLatch, LOW);
+}
+//int number = 20;
+int lang = 24;
+int kurz = 120;
+int prepare = 10;
+
+
+void loop()
+{
+
+  showNumber(999);
+  delay(100);
+  showNumber(888);
+  delay(100);
+  showNumber(777);
+  delay(100);
+  showNumber(666);
+  delay(100);
+  showNumber(555);
+  delay(100);
+  showNumber(444);
+  delay(100);
+   showNumber(333);
+  delay(100);
+  showNumber(222);
+  delay(100);
+  showNumber(111);
+  delay(100);
+  showNumber(000);
+  delay(3000);
+
+  
+  //hier muss die Abfrage der GPIOs rein um zu wissen welche Variable zählt 
+  int number = kurz;
+    {
+    for (int count = prepare; count > -1; count--)
+    {
+      showNumber(count); //Test pattern
+      //i--;
+      //number %= 1; //Reset x after 999
+      Serial.println(count); //For debugging
+      delay(500);
+    }
+  }
+  {
+    for (int count = number; count > -1; count--)
+    {
+      showNumber(count); //Test pattern
+      //i--;
+      //number %= 1; //Reset x after 999
+      Serial.println(count); //For debugging
+      delay(500);
+    }
+  }
+}
+
+//Takes a number and displays 2 numbers. Displays absolute value (no negatives)
+void showNumber(float value)
+{
+  int number = abs(value); //Remove negative signs and any decimals
+
+  //Serial.print("number: ");
+  //Serial.println(number);
+
+  for (byte x = 0 ; x < 3 ; x++)
+  {
+    int remainder = number % 10;
+
+    postNumber(remainder, false);
+
+    number /= 10;
+  }
+
+  //Latch the current segment data
+  digitalWrite(segmentLatch, LOW);
+  digitalWrite(segmentLatch, HIGH); //Register moves storage register on the rising edge of RCK
+}
+
+//Given a number, or '-', shifts it out to the display
+void postNumber(byte number, boolean decimal)
+{
+  //    -  A
+  //   / / F/B
+  //    -  G
+  //   / / E/C
+  //    -. D/DP
+
+#define a  1<<0
+#define b  1<<6
+#define c  1<<5
+#define d  1<<4
+#define e  1<<3
+#define f  1<<1
+#define g  1<<2
+#define dp 1<<7
+
+  byte segments;
+
+  switch (number)
+  {
+    case 1: segments = b | c; break;
+    case 2: segments = a | b | d | e | g; break;
+    case 3: segments = a | b | c | d | g; break;
+    case 4: segments = f | g | b | c; break;
+    case 5: segments = a | f | g | c | d; break;
+    case 6: segments = a | f | g | e | c | d; break;
+    case 7: segments = a | b | c; break;
+    case 8: segments = a | b | c | d | e | f | g; break;
+    case 9: segments = a | b | c | d | f | g; break;
+    case 0: segments = a | b | c | d | e | f; break;
+    case ' ': segments = 0; break;
+    case 'c': segments = g | e | d; break;
+    case '-': segments = g; break;
+  }
+
+  if (decimal) segments |= dp;
+
+  //Clock these bits out to the drivers
+  for (byte x = 0 ; x < 8 ; x++)
+  {
+    digitalWrite(segmentClock, LOW);
+    digitalWrite(segmentData, segments & 1 << (7 - x));
+    digitalWrite(segmentClock, HIGH); //Data transfers to the register on the rising edge of SRCK
+  }
+}
+
